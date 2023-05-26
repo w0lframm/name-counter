71dca4228189c35ea7411dff2f4822f0520fc321
kate-lebedeva <katty.papina@gmail.com>
2023-05-26 16:25:55 +0300

    Added basic reader-counter functionality
    added name list for tests and call from main.go

--- main.go
+++ main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	//"name-counter/cli"
+	"name-counter/reader-counter"
+)
+
+func main() {
+	//cli.Start()
+	reader_counter.ReadAndCount("C:\\Tmp\\name_counter\\names\\names.txt")
+}
--- reader-counter/reader-counter.go
+++ reader-counter/reader-counter.go
@@ -1,1 +1,31 @@
 package reader_counter
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadAndCount(path string) {
+	//
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("file %v opened successfully!!\n", file.Name())
+	defer file.Close()
+
+	names := make(map[string]int, 1000)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		names[scanner.Text()]++
+	}
+	printNames(names)
+}
+
+func printNames(names map[string]int) {
+	for name, freq := range names {
+		fmt.Printf("%s: %v\n", name, freq)
+	}
+	fmt.Println("done printing!")
+}
