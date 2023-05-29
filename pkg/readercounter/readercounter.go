08656da6c7db726a2ed85eea9aa3de69d25c49f4
kate-lebedeva <katty.papina@gmail.com>
2023-05-29 15:34:13 +0300

    Added comment to magic number in readercounter.go
    The size of a map explained.

--- pkg/readercounter/readercounter.go
+++ pkg/readercounter/readercounter.go
@@ -5,6 +5,9 @@ import (
 )
 
 func ReadCountNames(scanner *bufio.Scanner) map[string]int {
+	//According to B-001-031-676-ALL (1987) there are a total of 496 really used
+	//male and female names in RSFSR. The map of 1500 items should be enough to
+	//store unique names of modern St.Petersburg's residents and guests.
 	names := make(map[string]int, 1500)
 	for scanner.Scan() {
 		text := scanner.Text()
